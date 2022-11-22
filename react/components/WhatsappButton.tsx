d03b3a99b68081be2f726565349043e610676000
Miguel Foliaco <foliaco18@outlook.es>
2022-11-22 10:02:39 -0500

    FIX(ICON DEFAULT) se resolvio un problema con la imagen por defecto del boton

--- react/components/WhatsappButton.tsx
+++ react/components/WhatsappButton.tsx
@@ -1,20 +1,52 @@
-import React from "react";
-import logo from './IconWhatsapp.svg';
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { FaWhatsapp } from 'react-icons/fa'
 
 type Props = {
-  bgcolor: string;
-  icon?: string; //Url del icono
-  radius?: number; // border radius del elemento
-  phone: string; // Telefono al que se le va enviar el mensaje
-  message: string; // Mensaje a enviar
+  bgcolor: string
+  icon?: string // Url del icono
+  radius?: number // border radius del elemento
+  phone: string // Telefono al que se le va enviar el mensaje
+  message: string // Mensaje a enviar
 }
-const WhatsappButton = ({ bgcolor = '#0cdc4d', message = "Hola mundo", phone = '573111111111', radius = 50, icon }: Props) => {
-
+const WhatsappButton = ({
+  bgcolor = '#0cdc4d',
+  message = 'Hola mundo',
+  phone = '573111111111',
+  radius = 50,
+  icon,
+}: Props) => {
   return (
-    <div style={{ borderRadius: `${radius}px`, overflow: 'hidden', background: bgcolor, width: 50, height: 50, position: 'fixed', bottom: 30, right: 30, display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer' }}>
-      <a href={`https://wa.me/${phone}?text=${message.replace(/ /g, '%20')}`} target='_blank'>
-        <img style={{ width: 30, height: 30 }} src={icon === undefined ? logo : icon} />
+    <div
+      style={{
+        borderRadius: `${radius}px`,
+        overflow: 'hidden',
+        background: bgcolor,
+        width: 50,
+        height: 50,
+        position: 'fixed',
+        bottom: 30,
+        right: 30,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        cursor: 'pointer',
+      }}
+    >
+      <a
+        href={`https://wa.me/${phone}?text=${message.replace(/ /g, '%20')}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        {icon === undefined ? (
+          <FaWhatsapp color="white" style={{ width: 30, height: 30 }} />
+        ) : (
+          <img
+            style={{ width: 30, height: 30 }}
+            src={icon}
+            alt="Whatsapp button"
+          />
+        )}
       </a>
     </div>
   )
@@ -26,33 +58,33 @@ WhatsappButton.propTypes = {
   phone: PropTypes.string.isRequired,
   message: PropTypes.string,
   icon: PropTypes.string,
-};
+}
 
 WhatsappButton.schema = {
   title: 'Boton de WhatsApp',
-  type: "object",
+  type: 'object',
   properties: {
     icon: {
-      title: "Logo de WhatsApp que relacione con la marca",
-      type: "string",
+      title: 'Logo de WhatsApp que relacione con la marca',
+      type: 'string',
       widget: {
-        "ui:widget": "image-uploader"
-      }
+        'ui:widget': 'image-uploader',
+      },
     },
     phone: {
-      title: "Telefono",
-      description: "Agrega un numero de telefono",
-      type: "string"
+      title: 'Telefono',
+      description: 'Agrega un numero de telefono',
+      type: 'string',
     },
     message: {
-      title: "Mensaje",
-      description: "Agrega un mensaje para enviar",
-      type: "string",
+      title: 'Mensaje',
+      description: 'Agrega un mensaje para enviar',
+      type: 'string',
       widget: {
-        "ui:widget": "textarea"
-      }
-    }
-  }
+        'ui:widget': 'textarea',
+      },
+    },
+  },
 }
 
-export default WhatsappButton;
+export default WhatsappButton
